63fdb9bf5e126e7fb1d795b636230d5d10c72f77
Igor <igorvette@mail.ru>
2022-10-19 15:34:05 +0100

    Ошибка сборки

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте массив заполненный случайными положительными трехзначными числами. 
+// Напишите программу, которая покажет количество четных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2*/
+
+
+
+Printer(CreatArray(5));
+
+int [] CreatArray (int len)
+{
+    int [] array = new int [len];
+
+    for (int i=0; i<len; i++)
+    {
+        int rnd = new Random().Next(100, 1000);
+        array[i] = rnd;
+    }
+    return array;
+}
+
+void Printer(int[] array)
+{
+    int count = 0;
+    for (int = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+            count++;
+            Console.WriteLine($"{array[i]} ");
+        }
+        else{Console.Write($"{array[i]} ");}
+    }
+    Console.Write($" -> {count}");
+}
+
